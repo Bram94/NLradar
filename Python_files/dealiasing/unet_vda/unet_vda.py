Fix bug with tensorflow load_weignts + fix issue with data masking MF + prevent reloading bufr tables + max v_nyquist dealiasing setting

Python_files/dealiasing/unet_vda/unet_vda.py
@@ -1,13 +1,14 @@
 # Copyright (C) 2016-2024 Bram van 't Veen, bramvtveen94@hotmail.com
 # Distributed under the GNU General Public License version 3, see <https://www.gnu.org/licenses/>.
 
-import os
 import sys
 sys.path.append('src/')
 
 import numpy as np
 import time as pytime
 import tensorflow as tf
+tf.debugging.disable_traceback_filtering()
+
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 gpus = tf.config.experimental.list_physical_devices('GPU')
 print('detected GPUs:', gpus)
@@ -46,7 +47,7 @@ class Unet_VDA():
         self.vda = VelocityDealiaser(down, up)
         
         # Load weights
-        self.vda.load_weights(gv.programdir+'/Python_files/dealiasing/unet_vda/models/dealias_sn16_csi9764.SavedModel')
+        self.vda.load_weights(gv.programdir+'/Python_files/dealiasing/unet_vda/models/dealias_sn16_csi9764.SavedModel/variables/variables')
 
 
     def __call__(self, data, vn, azis=None, da=None, extra_dealias=True):

Python_files/nlr.py
@@ -114,6 +114,7 @@ productunfiltered={j:False for j in range(10)}
 polarization={j:'H' for j in range(10)}
 apply_dealiasing = {j:True for j in range(10)}
 dealiasing_setting = gv.velocity_dealiasing_settings[-1]
+dealiasing_max_nyquist_vel = 50
 dualprfdealiasing_n_it = 50
 cartesian_product_res = 1.
 scans=[1,2,3,4,5,1,2,3,4,5]
@@ -200,8 +201,8 @@ reset_volume_attributes = True #Gets set to False in nlr_datasourcegeneral.py
 
 
 
-variables_names_raw=['variables_resettodefault_version','reset_volume_attributes','radarsources_dirs','radardirs_additional','radardata_dirs','radardata_dirs_indices','derivedproducts_dir','derivedproducts_filename_version','radardata_product_versions','selected_product_versions_ordered','movefiles_parameters','radar','scan_selection_mode','date','time','current_case_list_name','current_case','cases_offset_minutes','cases_looping_speed','cases_animation_window','cases_use_case_zoom','cases_loop_subset','cases_loop_subset_ncases','animation_duration','animation_speed_minpsec','animation_hold_lastframe','desired_timestep_minutes','self.max_timestep_minutes','maxspeed_minpsec','dataset','products','productunfiltered','polarization','apply_dealiasing','dealiasing_setting','dualprfdealiasing_n_it','cartesian_product_res','scans','plot_mode','savefig_filename','savefig_include_menubar','animation_filename','ani_delay_ref','ani_delay','ani_delay_end','ani_sort_files','ani_group_datasets','ani_quality','networktimeout','minimum_downloadspeed','api_keys','stormmotion_save','pos_markers_latlons','pos_markers_latlons_save','use_storm_following_view','view_nearest_radar','radar_bands_view_nearest_radar','data_selected_startazimuth','show_vwp','include_sfcobs_vwp','vwp_manual_sfcobs','vwp_manual_axlim','vvp_range_limits','vvp_height_limits','vvp_vmin_mps','vwp_sigmamax_mps','vwp_shear_layers','vwp_vorticity_layers','vwp_srh_layers','vwp_sm_display','base_url_obs','cmaps_minvalues','cmaps_maxvalues','PP_parameter_values','PP_parameters_panels','max_radardata_in_memory_GBs','sleeptime_after_plotting','use_scissor','colortables_dirs_filenames','dimensions_main','fontsizes_main','bgcolor','panelbdscolor','bgmapcolor','mapvisibility','mapcolorfilter','maptiles_update_time','radar_markersize','radar_colors','lines_colors','lines_show','lines_width','lines_antialias','ghtext_show','grid_showtext','show_heightrings_derivedproducts','showgridheightrings_panzoom','showgridheightrings_panzoom_time','gridheightrings_fontcolor','gridheightrings_fontsize','grid_showtext']
-variables_names_withclassreference=['variables_resettodefault_version','self.reset_volume_attributes','self.radarsources_dirs','self.radardirs_additional','self.radardata_dirs','self.radardata_dirs_indices','self.derivedproducts_dir','self.derivedproducts_filename_version','self.radardata_product_versions','self.selected_product_versions_ordered','self.movefiles_parameters','self.crd.radar','self.crd.scan_selection_mode','self.crd.date','self.crd.time','self.current_case_list_name','self.current_case','self.cases_offset_minutes','self.cases_looping_speed','self.cases_animation_window','self.cases_use_case_zoom','self.cases_loop_subset','self.cases_loop_subset_ncases','self.animation_duration','self.animation_speed_minpsec','self.animation_hold_lastframe','self.desired_timestep_minutes','self.max_timestep_minutes','self.maxspeed_minpsec','self.crd.dataset','self.crd.products','self.crd.productunfiltered','self.crd.polarization','self.crd.apply_dealiasing','self.dealiasing_setting','self.dualprfdealiasing_n_it','self.cartesian_product_res','self.crd.scans','self.crd.plot_mode','self.savefig_filename','self.savefig_include_menubar','self.animation_filename','self.ani_delay_ref','self.ani_delay','self.ani_delay_end','self.ani_sort_files','self.ani_group_datasets','self.ani_quality','self.networktimeout','self.minimum_downloadspeed','self.api_keys','self.stormmotion_save','self.pos_markers_latlons','self.pos_markers_latlons_save','self.use_storm_following_view','self.view_nearest_radar','self.radar_bands_view_nearest_radar','self.data_selected_startazimuth','self.show_vwp','self.include_sfcobs_vwp','self.vwp_manual_sfcobs','self.vwp_manual_axlim','self.vvp_range_limits','self.vvp_height_limits','self.vvp_vmin_mps','self.vwp_sigmamax_mps','self.vwp_shear_layers','self.vwp_vorticity_layers','self.vwp_srh_layers','self.vwp_sm_display','self.base_url_obs','self.cmaps_minvalues','self.cmaps_maxvalues','self.PP_parameter_values','self.PP_parameters_panels','self.max_radardata_in_memory_GBs','self.sleeptime_after_plotting','self.use_scissor','self.colortables_dirs_filenames','self.dimensions_main','self.fontsizes_main','self.bgcolor','self.panelbdscolor','self.bgmapcolor','self.mapvisibility','self.mapcolorfilter','self.maptiles_update_time','self.radar_markersize','self.radar_colors','self.lines_colors','self.lines_show','self.lines_width','self.lines_antialias','self.ghtext_show','self.grid_showtext','self.show_heightrings_derivedproducts','self.showgridheightrings_panzoom','self.showgridheightrings_panzoom_time','self.gridheightrings_fontcolor','self.gridheightrings_fontsize','self.grid_showtext']
+variables_names_raw=['variables_resettodefault_version','reset_volume_attributes','radarsources_dirs','radardirs_additional','radardata_dirs','radardata_dirs_indices','derivedproducts_dir','derivedproducts_filename_version','radardata_product_versions','selected_product_versions_ordered','movefiles_parameters','radar','scan_selection_mode','date','time','current_case_list_name','current_case','cases_offset_minutes','cases_looping_speed','cases_animation_window','cases_use_case_zoom','cases_loop_subset','cases_loop_subset_ncases','animation_duration','animation_speed_minpsec','animation_hold_lastframe','desired_timestep_minutes','self.max_timestep_minutes','maxspeed_minpsec','dataset','products','productunfiltered','polarization','apply_dealiasing','dealiasing_setting','dealiasing_max_nyquist_vel','dualprfdealiasing_n_it','cartesian_product_res','scans','plot_mode','savefig_filename','savefig_include_menubar','animation_filename','ani_delay_ref','ani_delay','ani_delay_end','ani_sort_files','ani_group_datasets','ani_quality','networktimeout','minimum_downloadspeed','api_keys','stormmotion_save','pos_markers_latlons','pos_markers_latlons_save','use_storm_following_view','view_nearest_radar','radar_bands_view_nearest_radar','data_selected_startazimuth','show_vwp','include_sfcobs_vwp','vwp_manual_sfcobs','vwp_manual_axlim','vvp_range_limits','vvp_height_limits','vvp_vmin_mps','vwp_sigmamax_mps','vwp_shear_layers','vwp_vorticity_layers','vwp_srh_layers','vwp_sm_display','base_url_obs','cmaps_minvalues','cmaps_maxvalues','PP_parameter_values','PP_parameters_panels','max_radardata_in_memory_GBs','sleeptime_after_plotting','use_scissor','colortables_dirs_filenames','dimensions_main','fontsizes_main','bgcolor','panelbdscolor','bgmapcolor','mapvisibility','mapcolorfilter','maptiles_update_time','radar_markersize','radar_colors','lines_colors','lines_show','lines_width','lines_antialias','ghtext_show','grid_showtext','show_heightrings_derivedproducts','showgridheightrings_panzoom','showgridheightrings_panzoom_time','gridheightrings_fontcolor','gridheightrings_fontsize','grid_showtext']
+variables_names_withclassreference=['variables_resettodefault_version','self.reset_volume_attributes','self.radarsources_dirs','self.radardirs_additional','self.radardata_dirs','self.radardata_dirs_indices','self.derivedproducts_dir','self.derivedproducts_filename_version','self.radardata_product_versions','self.selected_product_versions_ordered','self.movefiles_parameters','self.crd.radar','self.crd.scan_selection_mode','self.crd.date','self.crd.time','self.current_case_list_name','self.current_case','self.cases_offset_minutes','self.cases_looping_speed','self.cases_animation_window','self.cases_use_case_zoom','self.cases_loop_subset','self.cases_loop_subset_ncases','self.animation_duration','self.animation_speed_minpsec','self.animation_hold_lastframe','self.desired_timestep_minutes','self.max_timestep_minutes','self.maxspeed_minpsec','self.crd.dataset','self.crd.products','self.crd.productunfiltered','self.crd.polarization','self.crd.apply_dealiasing','self.dealiasing_setting','self.dealiasing_max_nyquist_vel','self.dualprfdealiasing_n_it','self.cartesian_product_res','self.crd.scans','self.crd.plot_mode','self.savefig_filename','self.savefig_include_menubar','self.animation_filename','self.ani_delay_ref','self.ani_delay','self.ani_delay_end','self.ani_sort_files','self.ani_group_datasets','self.ani_quality','self.networktimeout','self.minimum_downloadspeed','self.api_keys','self.stormmotion_save','self.pos_markers_latlons','self.pos_markers_latlons_save','self.use_storm_following_view','self.view_nearest_radar','self.radar_bands_view_nearest_radar','self.data_selected_startazimuth','self.show_vwp','self.include_sfcobs_vwp','self.vwp_manual_sfcobs','self.vwp_manual_axlim','self.vvp_range_limits','self.vvp_height_limits','self.vvp_vmin_mps','self.vwp_sigmamax_mps','self.vwp_shear_layers','self.vwp_vorticity_layers','self.vwp_srh_layers','self.vwp_sm_display','self.base_url_obs','self.cmaps_minvalues','self.cmaps_maxvalues','self.PP_parameter_values','self.PP_parameters_panels','self.max_radardata_in_memory_GBs','self.sleeptime_after_plotting','self.use_scissor','self.colortables_dirs_filenames','self.dimensions_main','self.fontsizes_main','self.bgcolor','self.panelbdscolor','self.bgmapcolor','self.mapvisibility','self.mapcolorfilter','self.maptiles_update_time','self.radar_markersize','self.radar_colors','self.lines_colors','self.lines_show','self.lines_width','self.lines_antialias','self.ghtext_show','self.grid_showtext','self.show_heightrings_derivedproducts','self.showgridheightrings_panzoom','self.showgridheightrings_panzoom_time','self.gridheightrings_fontcolor','self.gridheightrings_fontsize','self.grid_showtext']
 
 #Variables that are reset to their default for the next update. Needs to be updated before every new update, 
 #and 'variables_resettodefault_version' should always be included!!!!! reset_volume_attributes maybe too.
@@ -376,6 +377,7 @@ class GUI(QWidget):
         self.ani_group_datasets = ani_group_datasets
         self.ani_quality = ani_quality
         self.dealiasing_setting = dealiasing_setting
+        self.dealiasing_max_nyquist_vel = dealiasing_max_nyquist_vel
         self.dualprfdealiasing_n_it = dualprfdealiasing_n_it
         self.cartesian_product_res = cartesian_product_res
         self.networktimeout=networktimeout
@@ -839,6 +841,9 @@ class GUI(QWidget):
         self.select_dealiasing_setting = QWidget()
         self.select_dealiasing_setting.setWindowTitle('Select dealiasing setting')
         
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel('Setting used for velocity dealiasing'))
+        
         self.dealiasing_settingsw = {}
         group = QButtonGroup()
         hbox = QHBoxLayout()
@@ -848,10 +853,13 @@ class GUI(QWidget):
             self.dealiasing_settingsw[j].toggled.connect(lambda state, j=j: self.change_dealiasing_setting(j))
             group.addButton(self.dealiasing_settingsw[j])
             hbox.addWidget(self.dealiasing_settingsw[j])
-            
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel('Setting used for velocity dealiasing'))
         layout.addLayout(hbox)
+        self.dealiasing_max_nyquist_velw = QLineEdit(str(ft.rifdot0(ft.r1dec(self.dealiasing_max_nyquist_vel*self.pb.scale_factors['v']))))
+        self.dealiasing_max_nyquist_velw.editingFinished.connect(self.change_dealiasing_max_nyquist_vel)
+        layout.addWidget(QLabel("Select maximum Nyquist velocity for which to apply the Unet VDA model. For higher Nyquist velocities"))
+        layout.addWidget(QLabel(f"the model will not be applied, since aliasing becomes unlikely. Unit: {self.pb.productunits['v']}."))
+        layout.addWidget(self.dealiasing_max_nyquist_velw)
+            
         self.select_dealiasing_setting.setLayout(layout)
         self.select_dealiasing_setting.resize(self.select_dealiasing_setting.sizeHint())
         self.select_dealiasing_setting.show()
@@ -862,6 +870,14 @@ class GUI(QWidget):
         if self.pb.firstplot_performed:
             panels_update = [j for j in self.pb.panellist if gv.i_p[self.crd.products[j]] == 'v']
             self.pb.set_newdata(panels_update)
+            
+    def change_dealiasing_max_nyquist_vel(self):
+        input_val = self.dealiasing_max_nyquist_velw.text()
+        number = ft.to_number(input_val)
+        if not number is None and number > 0:
+            self.dealiasing_max_nyquist_vel = number/self.pb.scale_factors['v']
+        else:
+            self.dealiasing_max_nyquist_velw.setText(str(ft.rifdot0(ft.r1dec(self.dealiasing_max_nyquist_vel*self.pb.scale_factors['v']))))
         
           
     def change_plainproducts_parameters(self):
@@ -1663,7 +1679,7 @@ class GUI(QWidget):
         self.set_stormmotion=QWidget()
         self.set_stormmotion.setWindowTitle('Storm motion')
         layout=QFormLayout()
-        self.sm_direction=QLineEdit(format(self.stormmotion[0], '.1f')); self.sm_speed=QLineEdit(format(self.stormmotion[1]*gv.scale_factors_velocities[self.pb.productunits['s']], '.1f'))
+        self.sm_direction=QLineEdit(format(self.stormmotion[0], '.1f')); self.sm_speed=QLineEdit(format(self.stormmotion[1]*self.pb.scale_factors['s'], '.1f'))
 
         layout.addRow(QLabel('A storm speed of 0 kts removes the storm motion from the title,'))
         layout.addRow(QLabel('except when displaying the storm-relative velocity.'))
@@ -1720,7 +1736,7 @@ class GUI(QWidget):
             if not number1 is None and not number2 is None:
                 sm_speed_mps = number2
                 if convert_units:
-                    sm_speed_mps /= gv.scale_factors_velocities[self.pb.productunits['s']]
+                    sm_speed_mps /= self.pb.scale_factors['s']
                 self.stormmotion = np.array([number1 % 360, sm_speed_mps])
                 if self.stormmotion[1] == 0.: 
                     self.stormmotion[0] = 0
@@ -1730,7 +1746,7 @@ class GUI(QWidget):
                     self.stormmotion_save = {'sm':self.stormmotion, 'radar':self.crd.radar}
             else:
                 self.sm_direction.setText(format(self.stormmotion[0], '.1f'))
-                self.sm_speed.setText(format(self.stormmotion[1]*gv.scale_factors_velocities[self.pb.productunits['s']], '.1f'))
+                self.sm_speed.setText(format(self.stormmotion[1]*self.pb.scale_factors['s'], '.1f'))
 
         if self.pb.firstplot_performed:
             redraw_panellist = [j for j in self.pb.panellist if self.crd.products[j]=='s' or self.crd.products[j] in gv.plain_products_correct_for_SM]
@@ -2185,9 +2201,9 @@ class GUI(QWidget):
             for j in self.dsg.scanangles_all['z']:
                 properties_text += 'Scan '+str(j)+': '+"%.2f" % self.dsg.scanangles_all_m['z'][j]+" \xb0, "+"%.1f" % self.dsg.radial_range_all['z'][j]+' km, '+"%.4f" % self.dsg.radial_res_all['z'][j]+' km, '
                 if not self.dsg.nyquist_velocities_all_mps[j] is None:
-                    properties_text+="%.1f" % (self.dsg.nyquist_velocities_all_mps[j]*gv.scale_factors_velocities[self.pb.productunits['v']])+' '+self.pb.productunits['v'] +\
-                    ', '+(("%.1f" % (self.dsg.low_nyquist_velocities_all_mps[j]*gv.scale_factors_velocities[self.pb.productunits['v']])+' '+self.pb.productunits['v'] +\
-                    ', '+"%.1f" % (self.dsg.high_nyquist_velocities_all_mps[j]*gv.scale_factors_velocities[self.pb.productunits['v']])+' '+self.pb.productunits['v']) \
+                    properties_text+="%.1f" % (self.dsg.nyquist_velocities_all_mps[j]*self.pb.scale_factors['v'])+' '+self.pb.productunits['v'] +\
+                    ', '+(("%.1f" % (self.dsg.low_nyquist_velocities_all_mps[j]*self.pb.scale_factors['v'])+' '+self.pb.productunits['v'] +\
+                    ', '+"%.1f" % (self.dsg.high_nyquist_velocities_all_mps[j]*self.pb.scale_factors['v'])+' '+self.pb.productunits['v']) \
                     if not self.dsg.low_nyquist_velocities_all_mps[j] is None else '--, --')
                 else: 
                     #If it was impossible to obtain the Nyquist velocities, then they are set to '--'

Python_files/nlr_datasourcegeneral.py
@@ -767,9 +767,9 @@ class DataSource_General():
                 ft.create_subdicts_if_absent(self.scanangles, [self.crd.radar, self.crd.date])
                 try:
                     # Mono PRF dealiasing normally is performed below, except when a special treatment is required, e.g. when more than one 
-                    # Nyquist velocity is used for the scan. In that case it is performed in nlr_importdata.py, where self.mono_PRF_dealiasing_performed
-                    # must be set to True.
-                    self.mono_PRF_dealiasing_performed = False
+                    # Nyquist velocity is used for the scan. In that case the function self.perform_mono_prf_dealiasing is called in nlr_importdata.py,
+                    # after which self.mono_prf_dealiasing_performed is set to True in this function.
+                    self.mono_prf_dealiasing_performed = False
                     
                     if j in self.scantimes:
                         before = self.data[j], self.scantimes[j], self.data_azimuth_offset[j], self.data_radius_offset[j]
@@ -787,12 +787,13 @@ class DataSource_General():
                             self.data[j], self.scantimes[j], self.data_azimuth_offset[j], self.data_radius_offset[j] = before
                             continue
                     
+                    v_nyquist = self.nyquist_velocities_all_mps[self.crd.scans[j]]
                     if gv.i_p[self.crd.products[j]] == 'v' and self.crd.apply_dealiasing[j] and 'Unet VDA' in self.gui.dealiasing_setting and\
-                    self.data[j].dtype == 'float32' and self.nyquist_velocities_all_mps[self.crd.scans[j]] not in (None, 999.) and\
-                    not self.mono_PRF_dealiasing_performed:
+                    self.data[j].dtype == 'float32' and v_nyquist not in (None, 999.) and v_nyquist <= self.gui.dealiasing_max_nyquist_vel and\
+                    not self.mono_prf_dealiasing_performed:
                         # A Nyquist velocity of 999. indicates that it could not be determined, while it is at least high enough to include
                         # the scan in operations that require a sufficiently high Nyquist velocity.
-                        self.perform_mono_PRF_dealiasing(j)
+                        self.data[j] = self.perform_mono_prf_dealiasing(j, self.data[j])
                 except Exception as e:
                     print(e, 'get_data, panel '+str(j))
                     traceback.print_exception(type(e), e, e.__traceback__)
@@ -843,14 +844,17 @@ class DataSource_General():
         # stats.print_stats(20)  
         return self.data_changed, self.total_files_size
     
-    def perform_mono_PRF_dealiasing(self, j): # j is the panel
+    def perform_mono_prf_dealiasing(self, j, data, vn=None, azis=None, da=None): # j is the panel
         if not hasattr(self, 'vda'):
             from dealiasing.unet_vda.unet_vda import Unet_VDA
             self.vda = Unet_VDA()
-        self.data[j][self.data[j] == self.pb.masked_values['v']] = np.nan
+        data[data == self.pb.masked_values['v']] = np.nan
         t = pytime.time()
-        self.data[j] = self.vda(self.data[j], self.nyquist_velocities_all_mps[self.crd.scans[j]], extra_dealias='extra' in self.gui.dealiasing_setting)
+        vn = self.nyquist_velocities_all_mps[self.crd.scans[j]] if vn is None else vn
+        data = self.vda(data, vn, azis, da, extra_dealias='extra' in self.gui.dealiasing_setting)
+        self.mono_prf_dealiasing_performed = True
         print(pytime.time()-t, 't unet vda')
+        return data
     
     def calculate_derived_with_tilts(self, j): # j is the panel
         """Currently only calculates SRV.

Python_files/nlr_importdata.py
@@ -1985,9 +1985,11 @@ class MeteoFrance_BUFR():
         # Sigma denotes the standard deviation of reflectivity
         self.product_order = {'z':0, 'v':2, 'c':1, 'd':2, 'p':3, 'sigma':4}
         # Offsets for 'v' and 'sigma' include a correction for the fact that integer data values represent the start of an interval,
-        # with the average being 0.5 higherround()
+        # with the average being 0.5 higher
         self.product_offset = {'z':-10.5, 'v':60, 'c':30.5, 'd':-9.85, 'p':0.5, 'sigma':0.125}
         self.product_scale = {'z':1., 'v':-0.5, 'c':1., 'd':0.1, 'p':1., 'sigma':0.25}
+        self.product_nbits = {'z':8, 'v':8, 'c':8, 'd':8, 'p':16}
+        self.product_maskvals = {'z':'minmax', 'v':'min', 'c':'max', 'd':'max', 'p':'max'}
             
 
     def get_file_content(self, filepath, product):
@@ -2054,7 +2056,13 @@ class MeteoFrance_BUFR():
         if slice:
             data = data[slice]
         
-        data_mask = (data == data.min()) | (data == data.max())
+        bounds = [offset, offset+scale*(2**self.product_nbits[product]-1)]
+        # For velocity offset represents the maximum product value
+        if self.product_maskvals[product] == 'minmax':
+            data_mask = (data == min(bounds)) | (data == max(bounds))
+        else:
+            data_mask = data == (min(bounds) if self.product_maskvals[product] == 'min' else max(bounds))
+            
         if i_p == 'v' and apply_dealiasing and not self.dsg.low_nyquist_velocities_all_mps[scan] is None:
             c_data = None
             try:
@@ -2285,24 +2293,7 @@ class NEXRAD_L2():
         vn_azis = self.file.get_nyquist_vel(scans=[scan_index])[-n_azi:]
         # Exclude Nyquist velocities of 0 as is the case for TDWR radars
         if i_p == 'v' and vn_azis[0] != 0. and not panel is None and self.crd.apply_dealiasing[panel] and 'Unet VDA' in self.gui.dealiasing_setting:
-            if not hasattr(self.dsg, 'vda'):
-                from cProfile import Profile
-                profiler = Profile()
-                profiler.enable() 
-
-                from dealiasing.unet_vda.unet_vda import Unet_VDA
-                self.dsg.vda = Unet_VDA()
-
-            t = pytime.time()
-            data = self.dsg.vda(data, vn_azis, azis, da, 'extra' in self.gui.dealiasing_setting)
-            if 'profiler' in locals():
-                profiler.disable()
-                import pstats
-                stats = pstats.Stats(profiler).sort_stats('cumtime')
-                stats.print_stats(50)  
-
-            print(pytime.time()-t, 't unet vda')
-            self.dsg.mono_PRF_dealiasing_performed = True
+            data = self.dsg.perform_mono_prf_dealiasing(panel, data, vn_azis, azis, da)
         
         data, azi_offset = map_onto_regular_grid(data, n_azi, azis, diffs, da, azi_offset)
         if not panel is None:
@@ -2609,18 +2600,11 @@ class CFRadial():
                 data *= 100.
             
             if i_p == 'v' and not j is None and self.crd.apply_dealiasing[j]:
-                if not hasattr(self.dsg, 'vda'):
-                    from dealiasing.unet_vda.unet_vda import Unet_VDA
-                    self.dsg.vda = Unet_VDA()
-                    
-                t = pytime.time()
                 vn = self.dsg.nyquist_velocities_all_mps[scan]
                 # vmax = np.abs(data).max()
                 # if vmax > vn:
                 #     vn = 26.2
-                data = self.dsg.vda(data, vn, azis, da)
-                print(pytime.time()-t, 't unet vda')
-                self.dsg.mono_PRF_dealiasing_performed = True
+                data = self.dsg.perform_mono_prf_dealiasing(j, data, vn, azis, da)
             
             self.dsg.data[j], azi_offset = map_onto_regular_grid(data, n_azi, azis, diffs, da, azi_offset, azi_pos='left')
             if not j is None:
@@ -2715,18 +2699,11 @@ class DORADE():
         data = data[-n_azi:]
                 
         if i_p == 'v' and not j is None and self.crd.apply_dealiasing[j]:
-            if not hasattr(self.dsg, 'vda'):
-                from dealiasing.unet_vda.unet_vda import Unet_VDA
-                self.dsg.vda = Unet_VDA()
-                
-            t = pytime.time()
             vn = self.dsg.nyquist_velocities_all_mps[scan]
             vmax = np.abs(data).max()
             if vmax > vn:
                 vn = vmax
-            data = self.dsg.vda(data, vn, azis, da)
-            print(pytime.time()-t, 't unet vda')
-            self.dsg.mono_PRF_dealiasing_performed = True
+            data = self.dsg.perform_mono_prf_dealiasing(j, data, vn, azis, da)
         
         self.dsg.data[j], azi_offset = map_onto_regular_grid(data, n_azi, azis, diffs, da, azi_offset, azi_pos='center')
         if not j is None: